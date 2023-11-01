5c4037fbdfdd75caf3ed715582c3d2f1acf08cc8
Ariful Islam <ariful.rony10@gmail.com>
2023-11-01 17:26:31 +0600

    Fix: Herbal and allopathic

--- src/app/features/home/components/products-by-category/products-by-category.component.ts
+++ src/app/features/home/components/products-by-category/products-by-category.component.ts
@@ -1,29 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from 'src/app/shared/models/product.model';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-products-by-category',
   templateUrl: './products-by-category.component.html',
   styleUrls: ['./products-by-category.component.scss'],
 })
-export class ProductsByCategoryComponent {
-  constructor(private productService: ProductService) {}
-
-  products: Product[] = [];
+export class ProductsByCategoryComponent implements OnInit {
+  herbalProducts: Product[] = [];
+  allopathyProducts: Product[] = [];
   isLoading: boolean = false;
 
+  constructor(private productService: ProductService) {}
+
   ngOnInit(): void {
-    isLoading: true;
-    this.productService.fetchProductsByType('herbal').subscribe({
-      next: (products) => {
-        this.products = products;
-        console.log(products);
+    this.isLoading = true;
+
+    forkJoin([
+      this.productService.fetchProductsByType('herbal'),
+      this.productService.fetchProductsByType('allopathic'),
+    ]).subscribe({
+      next: ([herbalProducts, allopathyProducts]) => {
+        this.herbalProducts = herbalProducts;
+        this.allopathyProducts = allopathyProducts;
       },
-      error: (err) => console.log(err),
+      error: (err) => {
+        console.error(err);
+      },
       complete: () => {
-        console.log('Completed');
         this.isLoading = false;
+        console.log('Completed');
+        console.log('Herbal', this.herbalProducts);
+        console.log('Herbal', this.allopathyProducts);
       },
     });
   }
